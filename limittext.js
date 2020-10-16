bc18e15f30b76650b5b4b0bcd7cfb07e856842ab
Mari <33049628+Melante@users.noreply.github.com>
2020-10-16 08:57:02 +0300

    Add files via upload

--- limittext.js
+++ limittext.js
@@ -1,4 +1,5 @@
  let button =document.querySelector('.message-button');
+ let commentWrite = document.querySelector('.comment-write');
  limit.oninput = limit.onkeyp = limit.onchange = function() {
   var len = this.value.length;
   if (len > this.max) {
@@ -7,10 +8,16 @@
   }
   else if (len<10){
     button.disabled = true;
+    commentWrite.classList.add('warning');
     ('Короткое сообщение');
+  }
+  else if (len>200){
+    button.disabled = true;
+    commentWrite.classList.add('warning');
   }
   else{
     button.disabled = false;
+    commentWrite.classList.remove('warning');
   }
   document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('max-' + this.id).innerHTML = 'Осталось: ' + (+this.max - len) + ' символов';
